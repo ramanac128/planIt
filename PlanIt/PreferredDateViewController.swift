77d7d57b2056253868b911798deeeab409bb77a5
Richmond Starbuck <rbstarbuck@gmail.com>
2016-11-01 17:01:11 -0400

    Created preferred date/time test module

---
+++ PlanIt/PreferredDateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PreferredDateViewController.swift
+//  PlanIt
+//
+//  Created by Richmond Starbuck on 11/1/16.
+//  Copyright © 2016 OneTwo Productions. All rights reserved.
+//
+
+import UIKit
+
+class PreferredDateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
